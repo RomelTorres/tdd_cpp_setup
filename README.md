92dae59e7cad01abc9ffe3ce5f865bbd953f3020
RomelTorres <torres.romel@gmail.com>
2017-07-05 19:13:07 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Environment setup script
+This is a simple setup script for setting up the environment needed to code the examples in the book
+** Modern C++ Programming with Test Driven Development ** by [Jeff Langr](https://github.com/jlangr)
+
+** More to come later..**
--- tdd_cpp_setup.sh
+++ tdd_cpp_setup.sh
@@ -9,13 +9,29 @@ else
     sudo apt-get install build-essential -y
     echo "About to install cmake"
     sudo apt-get install cmake -y
-    # From here we build googletests library
+    # Build googletests library
     export GMOCK_HOME=$PWD/googletest
     echo "Setting GMOCK_HOME variable to $GMOCK_HOME"
     echo "About to build google test"
     cd ./googletest
-    mkdir mybuild
+    mkdir -p mybuild
     cd mybuild
     cmake -G"Unix Makefiles" ..
+    make
+    # Build cpputest library
+    cd ../..
+    export CPPUTEST_HOME=$PWD/cpputest
+    echo "Setting CPPUTEST_HOME variable to $CPPUTEST_HOME"
+    echo "About to build cpputest"
+    cd ./cpputest/cpputest_build
+    cmake ..
+    make
+    # Build libcurl
+    cd ../..
+    export CURL_HOME=$PWD/curl
+    cd $CURL_HOME
+    mkdir -p build
+    cd build
+    cmake ..
     make
 fi
